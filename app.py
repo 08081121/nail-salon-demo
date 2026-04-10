27718259e29303dd91ad35926c8ef04c0347628d
08081121 <beecom0808@gmail.com>
2026-04-10 10:20:41 +0700

    app.py

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ def chat():
     return jsonify({"reply": reply})
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
